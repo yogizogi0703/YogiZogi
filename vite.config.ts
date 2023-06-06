66784ae42e0e22bd8e415d4b49be19e60d060dac
Ryomi-j <ryomi.j78@gmail.com>
2023-06-06 10:34:41 +0900

    🔧 build: rebuild after chaging hompage address

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: 'FE',
   plugins: [react()],
   test: {
     globals: true,
     setupFiles: ['./src/mocks/setupTests.ts'],
     environment: 'jsdom'
   },
-  define:{
-    'process.env.NODE_ENV': '"development"' 
+  define: {
+    'process.env.NODE_ENV': '"development"'
   }
 });
