615aa9fe0b9fa4cc17f3420c958a70b5c4c5514b
Ryomi-j <ryomi.j78@gmail.com>
2023-06-06 12:27:27 +0900

    ✨ feat: add function to select and show check-in and check-out date

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -2,12 +2,31 @@ import { BiMap } from 'react-icons/bi';
 import { FcCalendar } from 'react-icons/fc';
 import { BsPeople } from 'react-icons/bs';
 import { BsSearch } from 'react-icons/bs';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { ko } from 'date-fns/esm/locale';
 import { GetGeoInfo } from '../../utils/getGeoInfo';
+import { getFormatedDate } from '../../utils/getFormatedDate';
 
 export const SearchBar = () => {
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState('')
+  const [checkInDate, setCheckInDate] = useState(new Date());
+  const [checkOutDate, setCheckOutDate] = useState<Date | null>(null);
+  const [dateContent, setDateContent] = useState('');
+  const [calendarState, setCalendarState] = useState(false);
   const [userGeoInfo, setGeoInfo] = useState([37.57, 126.9]);
+
+    // 사용자가 check-in/check-out date를 선택하면, date를 형식(yyyy-mm-dd)에 맞게 변경
+    useEffect(() => {
+        if (checkOutDate) {
+          setDateContent(() => {
+            const selectedCheckInDate = getFormatedDate(checkInDate);
+            const selectedCheckOutDate = getFormatedDate(checkOutDate);
+            return `From ${selectedCheckInDate} to ${selectedCheckOutDate}`;
+          });
+        }
+      }, [checkInDate, checkOutDate]);
 
   return (
     <section className="flex border rounded-md w-2/3 mx-auto p-3">
@@ -30,13 +49,52 @@ export const SearchBar = () => {
           />
         </div>
       </div>
-
       <div className="w-1/3 relative">
-        <div>
+        <div onClick={() => setCalendarState(!calendarState)}>
           <div className="flex gap-1 items-center font-medium cursor-pointer">
             <FcCalendar /> Date
           </div>
-          <p className="text-sm cursor-pointer">Add Dates</p>
+          <p className="text-sm cursor-pointer">
+            {checkInDate && checkOutDate && !calendarState
+              ? dateContent
+              : 'Add Dates'}
+          </p>
+        </div>
+        <div
+          className={`flex gap-1 absolute left-1 p-3 rounded-lg bg-cyan-100 ${
+            calendarState ? 'block' : 'hidden'
+          }`}
+        >
+          <div>
+            Check In
+            <DatePicker
+              locale={ko}
+              inline
+              minDate={new Date()}
+              selected={checkInDate}
+              closeOnScroll={true}
+              onChange={(date: Date) => {
+                if (checkOutDate) setCheckOutDate(null);
+                setCheckInDate(date);
+              }}
+            />
+          </div>
+          <div>
+            Check Out
+            <DatePicker
+              locale={ko}
+              inline
+              minDate={new Date(checkInDate.getTime() + 86400000)}
+              selected={checkOutDate}
+              closeOnScroll={true}
+              onChange={(date: Date) => {
+                if (!checkInDate || checkInDate.getTime() >= date.getTime())
+                  return;
+                setCheckOutDate(date);
+                setCalendarState(!calendarState);
+              }}
+            />
+          </div>
         </div>
       </div>
       <div className="w-1/6">
