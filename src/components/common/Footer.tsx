65920cf25509dcb935ce3f7ae99c12e7086c14c0
cloudedpanther <76900250+cloudedpanther@users.noreply.github.com>
2023-06-07 17:55:37 +0900

    Feat/footer (#24)
    * ✨ feat: add logo components
    
    - add github and notion logo components to use in footer channel links
    
    * ✨ feat: add footer
    
    - add copyright and channel(github, notion) links
    - add tooltip to links
    - add responsive design
    
    * 🎨 style: change footer copyright font size

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,5 +1,48 @@
-const Footer = () => {
-  return <div>Footer</div>
-}
+import { Github, Notion } from '../logo';
+import { Link } from 'react-router-dom';
 
-export default Footer
+const channels = [
+  {
+    name: 'Github',
+    url: 'https://github.com/orgs/YOGIZOGI-Zerobase-2023/repositories',
+    logo: (
+      <Github
+        width={22}
+        height={22}
+        fill={'fill-gray-400 hover:fill-gray-500'}
+      />
+    )
+  },
+  {
+    name: 'Notion',
+    url: 'https://jet-enquiry-8e3.notion.site/YOGIZOGI-fd26b4d3490b431e8753780ebad4b6fc',
+    logo: (
+      <Notion
+        width={22}
+        height={22}
+        fill={'fill-gray-400 hover:fill-gray-500'}
+      />
+    )
+  }
+];
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-col justify-center md:justify-between items-center mx-16 my-8 md:flex-row">
+      <p className="text-gray-500 order-2 md:order-1 text-xs">
+        &copy; 2023 Yogizogi All Rights Reserved.
+      </p>
+      <div className="flex gap-8 order-1 md:order-2 mb-4 md:mb-0">
+        {channels.map((channel) => {
+          return (
+            <div key={channel.name} className="tooltip" data-tip={channel.name}>
+              <Link to={channel.url}>{channel.logo}</Link>
+            </div>
+          );
+        })}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
