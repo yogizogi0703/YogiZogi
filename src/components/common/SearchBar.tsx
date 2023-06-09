5960da84ce7f89fb101f53eb9c7dbd65704f2370
Kim Eunjeong <ryomi.j78@gmail.com>
2023-06-09 14:24:45 +0900

    :art:  feat: fix linux style error in window

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -50,14 +50,14 @@ export const SearchBar = () => {
   };
 
   return (
-    <section className="flex min-w-[300px] w-fit flex-col items-center lg:w-[1000px] md:flex-row border rounded-md max-w-5xl mx-auto p-3 shadow-md bg-white">
-      <div className="flex gap-2 max-w-full md:block md:w-1/2">
-        <p className="font-medium text-xs md:text-base min-w-fit">목적지</p>
-        <div className="flex gap-1 md:flex items-center">
+    <section className="flex flex-col sm:flex-row gap-5 md:gap-10 border min-w-fit w-auto max-w-4xl p-3 shadow-md mx-auto rounded-lg">
+      <div className="w-full md:w-1/2 flex flex-col gap-1">
+        <p className="font-semibold">목적지</p>
+        <div className="flex items-center justify-between gap-1 shrink bg-slate-200 rounded-md p-1">
           <input
             type="text"
-            placeholder="키워드 또는 시설명을 입력하세요"
-            className="input w-11/12 md:w-auto h-auto p-0 text-xs md:text-base focus:outline-none"
+            placeholder="검색어 입력하세요"
+            className="min-w-fit max-w-full lg:w-full h-auto p-0 input focus:outline-none bg-inherit"
             value={search.searchValue}
             onChange={(e) => {
               const newKeyword = e.target.value;
@@ -70,7 +70,7 @@ export const SearchBar = () => {
             }}
           />
           <BiMap
-            className="w-4 h-4 md:w-6 md:h-6 cursor-pointer"
+            className="cursor-pointer w-6 h-6"
             onClick={() => {
               GetGeoInfo(setSearch);
               handleSearchState('searchValue', '현재 위치에서 찾기');
@@ -78,94 +78,97 @@ export const SearchBar = () => {
           />
         </div>
       </div>
-      <div className="w-full md:w-1/6 relative">
-        <div
-          className="flex items-center gap-2 md:block"
-          onClick={() => setCalendarState(!calendarState)}
-        >
-          <div className="flex gap-1 items-center font-medium text-xs md:text-base cursor-pointer">
-            <FcCalendar /> 기간
+      <div className='w-full md:w-1/2 flex  justify-between'>
+        <div className="relative flex flex-col gap-2 w-1/2 sm:w-1/2">
+          <div className="flex items-center font-semibold gap-1">
+            <FcCalendar />
+            <span>기간</span>
           </div>
-          <p className="flex text-xs md:text-base cursor-pointer w-28 text-slate-500">
+          <p
+            className="cursor-pointer"
+            onClick={() => setCalendarState(!calendarState)}
+          >
             {dateContent !== '' && !calendarState
               ? dateContent
               : '날짜 선택하기'}
           </p>
+          <div
+            className={`p-3 rounded-lg bg-slate-300 absolute flex gap-1 top-14 ${
+              calendarState ? 'block' : 'hidden'
+            }`}
+          >
+            <div className="">
+              체크인
+              <DatePicker
+                locale={ko}
+                inline
+                minDate={new Date()}
+                selected={search.checkInDate}
+                closeOnScroll={true}
+                onChange={(date: Date) => {
+                  if (search.checkOutDate)
+                    handleSearchState('checkOutDate', new Date());
+                  handleSearchState('checkInDate', date);
+                }}
+              />
+            </div>
+            <div className="">
+              체크아웃
+              <DatePicker
+                locale={ko}
+                inline
+                minDate={new Date(search.checkInDate.getTime() + 86400000)}
+                selected={search.checkOutDate}
+                closeOnScroll={true}
+                onChange={(date: Date) => {
+                  if (
+                    !search.checkInDate ||
+                    search.checkInDate.getTime() >= date.getTime()
+                  )
+                    return;
+                  handleSearchState('checkOutDate', date);
+                  setCalendarState(!calendarState);
+                }}
+              />
+            </div>
+          </div>
         </div>
-        <div
-          className={`flex gap-1 absolute flex-col lg:flex-row md:left-0 md:top-14 p-3 rounded-lg bg-slate-300 ${
-            calendarState ? 'block' : 'hidden'
-          }`}
-        >
-          <div className="font-medium text-xs md:text-base">
-            체크인
-            <DatePicker
-              locale={ko}
-              inline
-              minDate={new Date()}
-              selected={search.checkInDate}
-              closeOnScroll={true}
-              onChange={(date: Date) => {
-                if (search.checkOutDate)
-                  handleSearchState('checkOutDate', new Date());
-                handleSearchState('checkInDate', date);
-              }}
-            />
+        <div className="flex flex-col gap-2 sm:w-1/4">
+          <div className="flex items-center font-semibold gap-1 ">
+            <BsPeople /> 인원
           </div>
-          <div className="font-medium text-xs md:text-base">
-            체크아웃
-            <DatePicker
-              locale={ko}
-              inline
-              minDate={new Date(search.checkInDate.getTime() + 86400000)}
-              selected={search.checkOutDate}
-              closeOnScroll={true}
-              onChange={(date: Date) => {
-                if (
-                  !search.checkInDate ||
-                  search.checkInDate.getTime() >= date.getTime()
-                )
-                  return;
-                handleSearchState('checkOutDate', date);
-                setCalendarState(!calendarState);
+          <p className="flex gap-3 items-center">
+            <button
+              className="btn btn-square btn-xs"
+              onClick={() => {
+                if (search.people === 0) return;
+                const peopleNum = search.people - 1;
+                handleSearchState('people', peopleNum);
               }}
-            />
-          </div>
+              disabled={search.people === 0}
+            >
+              -
+            </button>
+            {search.people}
+            <button
+              className="btn btn-square btn-xs"
+              onClick={() => {
+                const peopleNum = search.people + 1;
+                handleSearchState('people', peopleNum);
+              }}
+              disabled={search.people > 100}
+            >
+              +
+            </button>
+          </p>
+        </div>
+        <div className="flex justify-end items-center w-auto sm:w-1/4">
+          <button className="btn font-semibold md:w-full">
+            <BsSearch />
+            <span className="hidden md:block">검색</span>
+          </button>
         </div>
       </div>
-      <div className="flex gap-2 w-full md:ml-4 md:block md:w-1/6">
-        <div className="flex items-center gap-1 w-auto font-medium text-xs md:text-base">
-          <BsPeople /> 인원
-        </div>
-        <p className="flex items-center gap-2 text-xs text-center">
-          <button
-            className="btn btn-square w-4 h-4 min-h-[1rem] md:btn-xs rounded-lg"
-            onClick={() => {
-              if (search.people === 0) return;
-              const peopleNum = search.people - 1;
-              handleSearchState('people', peopleNum);
-            }}
-            disabled={search.people === 0}
-          >
-            -
-          </button>
-          {search.people}
-          <button
-            className="btn btn-square w-4 h-4 min-h-[1rem] min-h-4 md:btn-xs rounded-lg"
-            onClick={() => {
-              const peopleNum = search.people + 1;
-              handleSearchState('people', peopleNum);
-            }}
-            disabled={search.people > 100}
-          >
-            +
-          </button>
-        </p>
-      </div>
-      <button className="btn btn-sm mt-5 text-xs md:w-1/6 md:ml-3 md:btn-md md:mt-0">
-        <BsSearch />
-        검색
-      </button>
     </section>
   );
 };
