be75fa02fe811b97addff8cda47c52893c9c2b9e
cloudedpanther <76900250+cloudedpanther@users.noreply.github.com>
2023-06-09 20:47:21 +0900

    🎨 style: change main height for responsive design (#39)
    - change height of main component under 768px width

--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -6,11 +6,11 @@ const Main = () => {
       className="bg-no-repeat bg-cover"
       style={{ backgroundImage: 'url(/assets/images/main.png)' }}
     >
-      <div className="h-[calc(100vh-112px)] max-w-[1024px] m-auto flex flex-col justify-center">
+      <div className="h-[calc(100vh-144px)] md:h-[calc(100vh-112px)] max-w-[1024px] m-auto flex flex-col justify-center">
         <h1 className="mb-5 text-5xl sm:6xl md:text-8xl md:mb-16 font-semibold text-center">
           YogiZogi
         </h1>
-        <div className='p-3'>
+        <div className="p-3">
           <SearchBar />
         </div>
       </div>
