63585cb8af96519c96dbf4dc9149322eb57bc96a
Kim Eunjeong <ryomi.j78@gmail.com>
2023-06-12 18:24:36 +0900

    :memo:  chore: make 404.html and edit vite.config.ts and index.html for solving refresh issue after build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -11,5 +12,13 @@ export default defineConfig({
   },
   define: {
     'process.env.NODE_ENV': '"development"'
-  }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 });
