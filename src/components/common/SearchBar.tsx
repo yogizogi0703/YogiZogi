8d319a587f5bbaee9eb4ac2193aeead430f77e97
Kim Eunjeong <ryomi.j78@gmail.com>
2023-06-13 14:56:56 +0900

    :sparkles:  feat: make a function to close calendar when click outside

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -2,7 +2,7 @@ import { BiMap } from 'react-icons/bi';
 import { FcCalendar } from 'react-icons/fc';
 import { BsPeople } from 'react-icons/bs';
 import { BsSearch } from 'react-icons/bs';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { ko } from 'date-fns/esm/locale';
@@ -28,6 +28,7 @@ export const SearchBar = () => {
 
   const [dateContent, setDateContent] = useState('');
   const [calendarState, setCalendarState] = useState(false);
+  const calendarRef = useRef<HTMLDivElement>(null);
 
   // 사용자가 check-in/check-out date를 선택하면, date를 형식(yyyy-mm-dd)에 맞게 변경
   useEffect(() => {
@@ -48,6 +49,20 @@ export const SearchBar = () => {
       return { ...prev, [property]: value };
     });
   };
+
+  useEffect(() => {
+    const handleClickOutside = (e:MouseEvent) => {
+      if (calendarRef.current && !calendarRef.current.contains(e.target as Node)) {
+        setCalendarState(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
     <section className="flex flex-col sm:flex-row gap-5 md:gap-10 border min-w-fit w-auto max-w-4xl p-3 shadow-md mx-auto rounded-lg bg-white">
@@ -78,7 +93,7 @@ export const SearchBar = () => {
           />
         </div>
       </div>
-      <div className='w-full md:w-1/2 flex  justify-between'>
+      <div className="w-full md:w-1/2 flex  justify-between">
         <div className="relative flex flex-col gap-2 w-1/2 sm:w-1/2">
           <div className="flex items-center font-semibold text-lg gap-1">
             <FcCalendar />
@@ -93,6 +108,7 @@ export const SearchBar = () => {
               : '날짜 선택하기'}
           </p>
           <div
+            ref={calendarRef}
             className={`p-3 rounded-lg bg-slate-300 absolute flex flex-col lg:flex-row gap-1 top-14 ${
               calendarState ? 'block' : 'hidden'
             }`}
