8508184280ec8045e3abf762bfd708010a08423c
Kim Eunjeong <ryomi.j78@gmail.com>
2023-06-15 17:13:41 +0900

    :sparkles:  feat: make a function that returns date like yyyy-mm-dd

--- src/utils/handleDate.ts
+++ src/utils/handleDate.ts
@@ -8,3 +8,10 @@ export const getMonthDayFormat = (selectedDate: Date) => {
       : selectedDate.getDate();
   return `${month}/${date}`;
 };
+
+export const getDateFormat = (date: Date) => {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
