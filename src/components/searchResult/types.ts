f23c6a2329596e05ab75694dcd9c9f6a322ca553
cloudedpanther <rickeykoh@ajou.ac.kr>
2023-06-18 06:01:29 +0900

    ✨ feat: add types file for search result

---
+++ src/components/searchResult/types.ts
@@ -0,0 +1,5 @@
+import { categoryList, sortingFactorList } from './constants';
+
+export type SortingFactorTypes = (typeof sortingFactorList)[number];
+
+export type CategoryTypes = (typeof categoryList)[number];
