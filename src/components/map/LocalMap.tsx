4f6b98c5ef3210b017dbf7c278aba1750e2dd1e0
bbung <tjd123123@naver.com>
2023-06-19 15:15:01 +0900

    ✨feat: local marker placeURL 연동
    - marker 클릭시 시설 이름 표시 추가
    - 이름 클릭시 placeURL 새창으로 kakao map 검색페이지 연동

--- src/components/map/LocalMap.tsx
+++ src/components/map/LocalMap.tsx
@@ -1,6 +1,7 @@
 import { CustomOverlayMap, Map } from 'react-kakao-maps-sdk';
 import { LocalPlaceProps, PositionProps } from '../../api/map';
 import Marker from './marker/Marker';
+import { useState } from 'react';
 
 interface LocalMapProps {
   position: PositionProps;
@@ -9,17 +10,30 @@ interface LocalMapProps {
 }
 
 const LocalMap = ({ position, type, localData }: LocalMapProps) => {
+  const [center, setCenter] = useState<PositionProps>(position);
+  const [activeMarker, setActiveMarker] = useState<number>(0);
+
+  const handleOnClickMove = (position: PositionProps, id: number) => {
+    setCenter(position);
+    setActiveMarker(activeMarker === id ? 0 : id);
+  };
+
+  const handleCloseInfo = () => {
+    setActiveMarker(0);
+  };
+
   return (
     <div className="mx-auto max-w-5xl w-full h-full">
       <Map
-        center={position}
+        center={center}
         level={3}
         isPanto={true}
         style={{ width: '100%', height: '100%' }}
         disableDoubleClickZoom={type === 'mini' || false}
         draggable={type === 'full' || false}
+        onClick={handleCloseInfo}
       >
-        <CustomOverlayMap position={position}>
+        <CustomOverlayMap position={position} zIndex={99}>
           <Marker type={'home'} />
         </CustomOverlayMap>
         {type === 'full' &&
@@ -27,8 +41,14 @@ const LocalMap = ({ position, type, localData }: LocalMapProps) => {
             <CustomOverlayMap
               key={item.id}
               position={{ lat: item.y, lng: item.x }}
+              clickable={true}
             >
-              <Marker type={item.category_group_code} title={item.place_name} />
+              <Marker
+                type={item.category_group_code}
+                info={item}
+                isActive={activeMarker === item.id}
+                handleOnClickMove={handleOnClickMove}
+              />
             </CustomOverlayMap>
           ))}
       </Map>
--- src/components/map/marker/Marker.tsx
+++ src/components/map/marker/Marker.tsx
@@ -1,31 +1,41 @@
-import React, { useEffect, useState } from 'react';
 import HomeMarker from './HomeMarker';
 import ConvenienceMarker from './ConvenienceMarker';
 import TourMarker from './TourMarker';
 import StationMarker from './StationMarker';
+import { LocalPlaceProps, PositionProps } from '../../../api/map';
 
-const Marker = ({ type, title }: { type: string; title?: string }) => {
-  const [isShow, setIsShow] = useState<boolean>(false);
+interface MarkerProps {
+  type: string;
+  info?: LocalPlaceProps;
+  isActive?: boolean;
+  handleOnClickMove?: (arg1: PositionProps, arg2: number) => void;
+}
 
-  const handleShowInfo = (flag: boolean) => {
-    setIsShow(flag);
+const Marker = ({ type, info, isActive, handleOnClickMove }: MarkerProps) => {
+  const handleMarkerClick = () => {
+    if (info && handleOnClickMove) {
+      handleOnClickMove({ lat: info?.y, lng: info?.x }, info?.id);
+    }
+  };
+
+  const handleOpenPlaceWindow = () => {
+    window.open(info?.place_url, '_blank')!.focus();
   };
 
   return (
     <>
-      <div
-        onClick={() => handleShowInfo(!isShow)}
-        onMouseMove={() => handleShowInfo(true)}
-        onMouseLeave={() => handleShowInfo(false)}
-      >
+      <div onClick={handleMarkerClick}>
         {type === 'home' && <HomeMarker />}
         {type === 'CS2' && <ConvenienceMarker />}
         {type === 'AT4' && <TourMarker />}
         {type === 'SW8' && <StationMarker />}
       </div>
-      {isShow && title && (
-        <div className="absolute bottom-full p-2 font-semibold bg-white rounded-lg bg-opacity-100 shadow-md">
-          {title}
+      {isActive && info && (
+        <div
+          className="cursor-pointer absolute bottom-full p-2 font-semibold bg-white rounded-lg bg-opacity-100 shadow-md"
+          onClick={handleOpenPlaceWindow}
+        >
+          {info.place_name}
         </div>
       )}
     </>
