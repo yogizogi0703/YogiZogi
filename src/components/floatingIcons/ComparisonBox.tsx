8005ebb8fe561f9ec58bcfdb82a29224d3f16955
Kim Eunjeong <ryomi.j78@gmail.com>
2023-06-21 20:58:00 +0900

    :art:  style: apply prettier

--- src/components/floatingIcons/ComparisonBox.tsx
+++ src/components/floatingIcons/ComparisonBox.tsx
@@ -3,24 +3,23 @@ import { selectedAccommodation } from '../../store/atom/comparisonAtom';
 import { addCommasToPrice } from '../../helpers';
 import { useNavigate } from 'react-router-dom';
 
-
-export const ComparisonBox = ({ display }: {display: boolean}) => {
+export const ComparisonBox = ({ display }: { display: boolean }) => {
   const selectedAcc = useRecoilValue(selectedAccommodation);
   const navigate = useNavigate();
   const urlParams = new URLSearchParams(
     '?' + window.location.hash.split('?')[1]
-    );
-    const {
-      checkindate: checkInDate,
-      checkoutdate: checkOutDate,
-      people: people
+  );
+  const {
+    checkindate: checkInDate,
+    checkoutdate: checkOutDate,
+    people: people
   } = Object.fromEntries(urlParams.entries());
 
   const convertDateFormat = (date: string) => {
     const [, month, day] = date.split('-');
     return `${month}/${day}`;
   };
-  
+
   return (
     <article
       className={`flex flex-col justify-between gap-2 py-2 px-2 absolute w-60 h-52 bottom-0 right-16 rounded-xl bg-[#3abff8] ${
@@ -34,10 +33,7 @@ export const ComparisonBox = ({ display }: {display: boolean}) => {
       )}
       {selectedAcc.map((el, idx) => {
         return (
-          <div
-            key={idx}
-            className="flex gap-1 rounded-lg bg-white"
-          >
+          <div key={idx} className="flex gap-1 rounded-lg bg-white">
             <figure
               className="w-1/2 rounded-s-lg cursor-pointer tooltip tooltip-warning tooltip-right"
               data-tip="자세히보기"
@@ -64,7 +60,7 @@ export const ComparisonBox = ({ display }: {display: boolean}) => {
           </div>
         );
       })}
-      <div className='flex justify-end w-full h-full items-end'>
+      <div className="flex justify-end w-full h-full items-end">
         <button className="btn btn-xs bg-white">비교하기</button>
       </div>
     </article>
