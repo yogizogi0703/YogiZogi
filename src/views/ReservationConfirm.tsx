d8388f0425952eedcea0fc03510c6b97d27e25c5
cloudedpanther <rickeykoh@ajou.ac.kr>
2023-06-21 15:53:04 +0900

    ✨ feat: add banner to reservation confirm page

--- src/views/ReservationConfirm.tsx
+++ src/views/ReservationConfirm.tsx
@@ -1,4 +1,13 @@
 const ReservationConfirm = () => {
-  return <div>ReservationConfirm</div>
-}
-export default ReservationConfirm
+  return (
+    <div className="w-full">
+      <div className="w-full h-36 bg-[url('/assets/images/banner.png')] bg-cover bg-center relative z-[1]">
+        <div className="absolute top-0 left-0 w-full h-36 bg-emerald-500 opacity-40 z-[2]"></div>
+        <p className="absolute top-1/2 -translate-y-2/4 left-1/2 -translate-x-2/4 text-white text-3xl font-bold z-[3]">
+          예약 확인
+        </p>
+      </div>
+    </div>
+  );
+};
+export default ReservationConfirm;
