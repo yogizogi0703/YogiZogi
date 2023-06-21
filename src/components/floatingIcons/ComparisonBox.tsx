db73712e45c057b9ede7d7eb571f183aa6200f9f
Kim Eunjeong <ryomi.j78@gmail.com>
2023-06-21 18:42:53 +0900

    :memo:  chore: render selected accommodations data

--- src/components/floatingIcons/ComparisonBox.tsx
+++ src/components/floatingIcons/ComparisonBox.tsx
@@ -1,22 +1,52 @@
-export const ComparisonBox = ({ display }: { display: boolean }) => {
+import { useRecoilValue } from 'recoil';
+import { selectedAccommodation } from '../../store/atom/comparisonAtom';
+import { addCommasToPrice } from '../../helpers';
 
+export const ComparisonBox = ({ display }: {display: boolean}) => {
+  const selectedAcc = useRecoilValue(selectedAccommodation);
+  const urlParams = new URLSearchParams(
+    '?' + window.location.hash.split('?')[1]
+    );
+    const {
+      checkindate: checkInDate,
+      checkoutdate: checkOutDate,
+      people: people
+  } = Object.fromEntries(urlParams.entries());
+ 
   return (
     <article
       className={`flex flex-col justify-between gap-2 py-2 px-2 absolute w-60 h-52 bottom-0 right-16 rounded-xl bg-[#3abff8] ${
         display ? 'block' : 'hidden'
       }`}
     >
-      <div className="flex gap-1 rounded-lg bg-white">
-        <figure className="w-1/2 rounded-s-lg cursor-pointer">
-          <img className="rounded-s-lg h-full" />
-        </figure>
-        <div className="w-1/2">
-          <p className="font-semibold">이름</p>
-          <p>날짜 </p>
-          <p>가격</p>
-        </div>
-      </div>
-      <div className="flex justify-end">
+      {selectedAcc.map((el, idx) => {
+        return (
+          <div
+            key={idx}
+            className="flex gap-1 rounded-lg bg-white"
+          >
+            <figure
+              className="w-1/2 rounded-s-lg cursor-pointer tooltip tooltip-warning tooltip-right"
+              data-tip="자세히보기"
+            >
+              <img
+                src={el.pictureUrlList[0]}
+                alt={`${el.accommodationName} image`}
+                className="rounded-s-lg h-full"
+              />
+            </figure>
+            <div className="w-1/2">
+              <p className="truncate font-semibold">{el.accommodationName}</p>
+              <p>
+                {checkInDate} ~{' '}
+                {checkOutDate}
+              </p>
+              <p>{addCommasToPrice(el.price)}원</p>
+            </div>
+          </div>
+        );
+      })}
+      <div className='flex justify-end'>
         <button className="btn btn-sm bg-white">비교하기</button>
       </div>
     </article>
