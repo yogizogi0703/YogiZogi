10eaecf89d3f93cf0212f7c781cbff73771c6f2a
Kim Eunjeong <ryomi.j78@gmail.com>
2023-06-30 00:14:48 +0900

    :memo:  chore: fix room image error - add condition to render room images

--- src/components/accommodationDetail/RoomInfo.tsx
+++ src/components/accommodationDetail/RoomInfo.tsx
@@ -23,85 +23,90 @@ export const RoomInfo = ({
 
   return (
     <>
-      {roomInfo.map((el, idx) => {
-        return (
-          <div
-            key={idx}
-            className="flex flex-col items-center sm:flex-row gap-3"
-          >
-            <label
-              key={idx}
-              htmlFor="reservationModal"
-              className="flex"
-              onClick={() =>
-                setModalProps({
-                  imgList: el.pictureUrlList,
-                  alt: 'accommodation detail image',
-                  selectedImg: idx
-                })
-              }
-            >
-              <figure className="mx-auto cursor-pointer">
-                <img
-                  src={el.pictureUrlList[0].url}
-                  alt={`${accommodationName}-${el.roomName} image`}
-                />
-              </figure>
-            </label>
-            <div className="flex flex-col flex-wrap ml-2 w-fit sm:w-1/3">
-              <h3 className="text-base md:text-xl font-semibold md:mb-1">
-                {el.roomName}
-              </h3>
-              <div className="flex sm:flex-col gap-2">
-                <p>
-                  <span className="font-semibold">체크인: </span>
-                  {el.checkInTime}시
-                </p>
-                <p>
-                  <span className="font-semibold">체크아웃</span>:{' '}
-                  {el.checkOutTime}시
-                </p>
-                <p>
-                  <span className="font-semibold">기본인원</span>:{' '}
-                  {el.defaultPeople}명
-                </p>
-                <p>
-                  <span className="font-semibold">최대인원</span>:{' '}
-                  {el.maxPeople}명
-                </p>
-              </div>
-            </div>
-            <div className="divider divider-horizontal mt-[-12px]" />
-            <div className="flex flex-row sm:w-1/3 justify-center">
-              <div className="flex sm:flex-col gap-3 my-auto items-center">
-                <div className="font-semibold text-lg">
-                  {addCommasToPrice(el.price)}원
-                </div>
-                <button
-                  className="flex gap-2 btn btn-sm text-xs md:btn-md md:text-base bg-red-500 hover:bg-red-600 text-white"
-                  onClick={() => {
-                    setRoomData((prev) => ({
-                      ...prev,
-                      roomId: el.id,
-                      roomName: el.roomName,
-                      roomImg: el.pictureUrlList[0].url,
-                      price: el.price
-                    }));
-                    setModalState(true);
-                  }}
+      {roomInfo.length > 0 &&
+        roomInfo.map((el, idx) => {
+          return (
+            <>
+              {el.pictureUrlList.length > 0 && (
+                <div
+                  key={idx}
+                  className="flex flex-col items-center sm:flex-row gap-3"
                 >
-                  예약하기
-                </button>
-                <ConfirmModal
-                  data={roomData}
-                  modalState={modalState}
-                  setModalState={setModalState}
-                />
-              </div>
-            </div>
-          </div>
-        );
-      })}
+                  <label
+                    key={idx}
+                    htmlFor="reservationModal"
+                    className="flex"
+                    onClick={() =>
+                      setModalProps({
+                        imgList: el.pictureUrlList,
+                        alt: 'accommodation detail image',
+                        selectedImg: idx
+                      })
+                    }
+                  >
+                    <figure className="mx-auto cursor-pointer">
+                      <img
+                        src={el.pictureUrlList[0].url}
+                        alt={`${accommodationName}-${el.roomName} image`}
+                      />
+                    </figure>
+                  </label>
+                  <div className="flex flex-col flex-wrap ml-2 w-fit sm:w-1/3">
+                    <h3 className="text-base md:text-xl font-semibold md:mb-1">
+                      {el.roomName}
+                    </h3>
+                    <div className="flex sm:flex-col gap-2">
+                      <p>
+                        <span className="font-semibold">체크인: </span>
+                        {el.checkInTime}시
+                      </p>
+                      <p>
+                        <span className="font-semibold">체크아웃</span>:{' '}
+                        {el.checkOutTime}시
+                      </p>
+                      <p>
+                        <span className="font-semibold">기본인원</span>:{' '}
+                        {el.defaultPeople}명
+                      </p>
+                      <p>
+                        <span className="font-semibold">최대인원</span>:{' '}
+                        {el.maxPeople}명
+                      </p>
+                    </div>
+                  </div>
+                  <div className="divider divider-horizontal mt-[-12px]" />
+                  <div className="flex flex-row sm:w-1/3 justify-center">
+                    <div className="flex sm:flex-col gap-3 my-auto items-center">
+                      <div className="font-semibold text-lg">
+                        {addCommasToPrice(el.price)}원
+                      </div>
+                      <button
+                        className="flex gap-2 btn btn-sm text-xs md:btn-md md:text-base bg-red-500 hover:bg-red-600 text-white"
+                        onClick={() => {
+                          setRoomData((prev) => ({
+                            ...prev,
+                            roomId: el.id,
+                            roomName: el.roomName,
+                            roomImg: el.pictureUrlList[0].url,
+                            price: el.price
+                          }));
+                          setModalState(true);
+                        }}
+                      >
+                        예약하기
+                      </button>
+                      <ConfirmModal
+                        data={roomData}
+                        modalState={modalState}
+                        setModalState={setModalState}
+                      />
+                    </div>
+                  </div>
+                </div>
+              )}
+            </>
+          );
+        })}
     </>
   );
 };
