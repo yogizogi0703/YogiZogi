8875949f7f2f0d8fdb893b432419d6fe80776330
Kim Eunjeong <ryomi.j78@gmail.com>
2023-06-30 21:30:50 +0900

    :memo:  chore: fix typo and erase unnecessary code

--- src/components/floatingIcons/FloatingIcon.tsx
+++ src/components/floatingIcons/FloatingIcon.tsx
@@ -7,24 +7,23 @@ import { selectedAccommodation } from '../../store/atom/comparisonAtom';
 
 export const FloatingIcon = () => {
   const selectedAcc = useRecoilValue(selectedAccommodation);
-  const [chatState, setChatState] = useState(false);
-  const [comparisonState, setComparisonChatState] = useState(false);
+  const [comparisonState, setComparisonState] = useState(false);
 
   return (
     <div className="fixed bottom-[5%] flex flex-col gap-1 md:gap-5 right-1 sm:right-5 md:right-10 z-20">
-      <div
-        onClick={() => setChatState(!chatState)}
-        className="btn btn-warning md:w-16 md:h-16 rounded-[50%]"
-      >
-        <MdOutlineBedroomParent className="md:w-6 md:h-6" />
+      <div className="indicator">
+        <button className="btn btn-warning md:w-16 md:h-16 rounded-[50%]">
+          <MdOutlineBedroomParent className="md:w-6 md:h-6" />
+        </button>
       </div>
-      {/* <Chat display={chatState} /> */}
       <div className="indicator">
         <span className="indicator-item badge bg-red-500 text-white w-4 h-4 md:w-6 md:h-6 right-3 top-3 text-[10px] md:text-base border-none">
           {selectedAcc.length}
         </span>
         <button
-          onClick={() => setComparisonChatState(!comparisonState)}
+          onClick={() => {
+            setComparisonState(!comparisonState);
+          }}
           className="btn btn-info md:w-16 md:h-16 rounded-[50%]"
         >
           <BsBuilding className="md:w-6 md:h-6" />
