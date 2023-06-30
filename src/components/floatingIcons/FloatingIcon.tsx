57080d2a0faba4d6e5100899e5af62829c01c4b4
Kim Eunjeong <ryomi.j78@gmail.com>
2023-06-30 21:39:18 +0900

    :memo:  chore: add RoomComparisonBox to floating icon

--- src/components/floatingIcons/FloatingIcon.tsx
+++ src/components/floatingIcons/FloatingIcon.tsx
@@ -4,18 +4,26 @@ import { useState } from 'react';
 import { ComparisonBox } from './comparison/ComparisonBox';
 import { useRecoilValue } from 'recoil';
 import { selectedAccommodation } from '../../store/atom/comparisonAtom';
+import { RoomComparisonBox } from './comparison/RoomComparisonBox';
 
 export const FloatingIcon = () => {
   const selectedAcc = useRecoilValue(selectedAccommodation);
+  const [roomComparisonState, setRoomComparisonState] = useState(false);
   const [comparisonState, setComparisonState] = useState(false);
 
   return (
     <div className="fixed bottom-[5%] flex flex-col gap-1 md:gap-5 right-1 sm:right-5 md:right-10 z-20">
       <div className="indicator">
-        <button className="btn btn-warning md:w-16 md:h-16 rounded-[50%]">
+        <button
+          onClick={() => {
+            setRoomComparisonState(!roomComparisonState);
+          }}
+          className="btn btn-warning md:w-16 md:h-16 rounded-[50%]"
+        >
           <MdOutlineBedroomParent className="md:w-6 md:h-6" />
         </button>
       </div>
+      <RoomComparisonBox display={roomComparisonState} />
       <div className="indicator">
         <span className="indicator-item badge bg-red-500 text-white w-4 h-4 md:w-6 md:h-6 right-3 top-3 text-[10px] md:text-base border-none">
           {selectedAcc.length}
