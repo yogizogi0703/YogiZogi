847972680c34cf9db18e961682e55980dcc8f444
Kim Eunjeong <ryomi.j78@gmail.com>
2023-07-02 01:45:14 +0900

    :memo:  chore: add source props and handle comparison box by source page - room and accommodation

--- src/components/floatingIcons/FloatingIcon.tsx
+++ src/components/floatingIcons/FloatingIcon.tsx
@@ -7,13 +7,12 @@ import {
   selectedAccommodation,
   selectedRoom
 } from '../../store/atom/comparisonAtom';
-import { RoomComparisonBox } from './comparison/RoomComparisonBox';
 
 export const FloatingIcon = () => {
   const selectedAcc = useRecoilValue(selectedAccommodation);
   const selectedRooms = useRecoilValue(selectedRoom);
-  const [roomComparisonState, setRoomComparisonState] = useState(false);
   const [comparisonState, setComparisonState] = useState(false);
+  const [source, setSource] = useState('');
 
   return (
     <div className="fixed bottom-[5%] flex flex-col gap-1 md:gap-5 right-1 sm:right-5 md:right-10 z-20">
@@ -23,34 +22,39 @@ export const FloatingIcon = () => {
         </span>
         <button
           onClick={() => {
-            if (!roomComparisonState) {
+            if (source === 'room') {
               setComparisonState(false);
+              setSource('');
+            } else {
+              setComparisonState(true);
+              setSource('room');
             }
-            setRoomComparisonState(!roomComparisonState);
           }}
-          className="btn btn-warning md:w-16 md:h-16 rounded-[50%]"
+          className="btn bg-white border-red-500 md:w-16 md:h-16 rounded-[50%]"
         >
           <MdOutlineBedroomParent className="md:w-6 md:h-6" />
         </button>
       </div>
-      <RoomComparisonBox display={roomComparisonState} />
       <div className="indicator">
         <span className="indicator-item badge bg-red-500 text-white w-4 h-4 md:w-6 md:h-6 right-3 top-3 text-[10px] md:text-base border-none">
           {selectedAcc.length}
         </span>
         <button
           onClick={() => {
-            if (!comparisonState) {
-              setRoomComparisonState(false);
+            if (source === 'accommodation') {
+              setComparisonState(false);
+              setSource('')
+            } else {
+              setComparisonState(true);
+              setSource('accommodation');
             }
-            setComparisonState(!comparisonState);
           }}
-          className="btn btn-info md:w-16 md:h-16 rounded-[50%]"
+          className="btn bg-white border-red-500 md:w-16 md:h-16 rounded-[50%]"
         >
           <BsBuilding className="md:w-6 md:h-6" />
         </button>
       </div>
-      <ComparisonBox display={comparisonState} />
+      <ComparisonBox display={comparisonState} source={source} />
     </div>
   );
 };
