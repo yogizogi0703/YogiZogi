8ac7b4a452a88c612dcb65810a77cab510caa76d
yogizogi0703 <yogizogi0703@gmail.com>
2023-07-03 17:31:22 +0000

    ☔ build: fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FE',
+  base: '/',
   test: {
     globals: true,
     setupFiles: ['./src/mocks/setupTests.ts'],
