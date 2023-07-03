729fc0374c9e7186c0fe0e7a4e460240105b76c8
yogizogi0703 <yogizogi0703@gmail.com>
2023-07-03 17:20:29 +0000

    🚑 fix: resolve vercel deployment type error

--- src/mocks/msw.test.ts
+++ src/mocks/msw.test.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { describe, expectTypeOf, it } from 'vitest';
+import { describe, expect, expectTypeOf, it } from 'vitest';
 
 describe('MSW Fetch Test: ', () => {
   it('login', async () => {
