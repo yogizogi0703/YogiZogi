9697ac5209f3050aba98987cd531eb3689276b6f
yogizogi0703 <yogizogi0703@gmail.com>
2023-07-06 04:54:57 +0900

    🚑 fix: fix pwa bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
   base: '/',
   test: {
     globals: true,
     setupFiles: ['./src/mocks/setupTests.ts'],
-    environment: 'jsdom'
+    environment: 'jsdom',
   },
   define: {
-    'process.env.NODE_ENV': '"development"'
+    'process.env.NODE_ENV': '"development"',
   },
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        404: resolve(__dirname, 'public/404.html')
-      }
-    }
-  }
+        404: resolve(__dirname, 'public/404.html'),
+      },
+    },
+  },
 });
